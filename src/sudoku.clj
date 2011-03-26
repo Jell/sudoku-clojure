7b52372d3ea55f1096be8fbe836301093566f721
Jell <jean-louis@icehouse.se>
2011-03-26 22:46:45 +0100

    refactored.

--- src/sudoku.clj
+++ src/sudoku.clj
@@ -11,19 +11,19 @@
                '(0 4 0 0 0 0 0 0 7)
                '(0 0 7 0 0 0 3 0 0)])
 
+(def GRID_WIDTH 9)
+(def SUB_GRID_WIDTH 3)
+
+(defn pretty-matrix [m]
+  (doseq [row m] (println row)))
+
 (defn normalize-matrix [m]
   (matrix (matrix-map #(if (zero? %) 0 1) m)))
 
-(defn values-for-mask [m, mask]
-  (map #(int %) (filter #(not (zero? %)) (flatten (to-list (mult m mask))))))
-
-(defn available-values [m, mask]
-  (apply disj (cons (set (range 1 10)) (values-for-mask m mask))))
-
 (defn line-mask [i]
-  (concat (replicate (- i 1) (replicate 9 0))
-          (replicate 1       (replicate 9 1))
-          (replicate (- 9 i) (replicate 9 0))))
+  (concat (replicate i (replicate GRID_WIDTH 0))
+          (replicate 1 (replicate GRID_WIDTH 1))
+          (replicate (- GRID_WIDTH 1 i) (replicate GRID_WIDTH 0))))
 
 (defn vertical-mask [row]
   (apply bind-columns (line-mask row)))
@@ -32,8 +32,8 @@
   (apply bind-rows (line-mask column)))
 
 (defn square-range [row]
-  (let [i (+ 1 (* 3 (int (/ (- row 1) 3))))]
-  (range i (+ i 3))))
+  (let [i (* SUB_GRID_WIDTH (int (/ row SUB_GRID_WIDTH)))]
+  (range i (+ i SUB_GRID_WIDTH))))
 
 (defn square-mask [row, column]
   (mult (apply plus (map vertical-mask (square-range column)))
@@ -44,34 +44,37 @@
                           (horizontal-mask row)
                           (vertical-mask column))))
 
-(defn available-values-for-cell [m, row, column]
-  (available-values (matrix m) (mask-for row column)))
+(defn values-for-mask [m, mask]
+  (map #(int %) (filter #(not (zero? %)) (flatten (to-list (mult m mask))))))
 
-(defn constraints-for-cell [m ,row, column]
-  (- 9 (count (available-values-for-cell m row column))))
+(defn available-values-for-mask [m, mask]
+  (apply disj (cons (set (take GRID_WIDTH (iterate inc 1))) (values-for-mask m mask))))
 
 (defn get-cell [m, row, column]
-  (nth (nth m (- row 1)) (- column 1)))
+  (nth (nth m row) column))
 
 (defn set-cell [m, cell, new-value]
-  (let [row (- (first cell) 1) column (- (last cell) 1)]
+  (let [row (first cell) column (last cell)]
   (map-indexed  (fn [i, row-vector]
                   (if (== row i)
-                      (map-indexed  (fn [j, value]
-                                      (if (== column j)
-                                          new-value
-                                          value))
+                      (map-indexed  (fn [j, value] (if (== column j) new-value value))
                                     row-vector)
                       row-vector))
                 m)))
 
+(defn available-values-for-cell [m, row, column]
+  (available-values-for-mask (matrix m) (mask-for row column)))
+
+(defn constraints-for-cell [m ,row, column]
+  (- GRID_WIDTH (count (available-values-for-cell m row column))))
+
 (defn constraint-matrix [m]
   (map-indexed  (fn [row, row-vector]
                   (map-indexed  (fn [column, value]
-                                  [ (+ row 1)
-                                    (+ column 1)
-                                    (if (zero? (get-cell m (+ row 1) (+ column 1)))
-                                        (constraints-for-cell m (+ row 1) (+ column 1))
+                                  [ row
+                                    column
+                                    (if (zero? (get-cell m row column))
+                                        (constraints-for-cell m row column)
                                         -1)])
                                 row-vector))
                 m))
@@ -82,9 +85,6 @@
 (defn empty-cells-ordered-by-contraints [m]
   (map #(take 2 %) (sort-by #(nth % 2) > (empty-cells m))))
 
-(defn pretty-matrix [m]
-  (doseq [row m] (println row)))
-
 (defn sudoku [m]
   (pretty-matrix m)
   (println "\n----------------\n")
@@ -94,16 +94,6 @@
       (let [availables (apply available-values-for-cell (cons m cell))]
       (if (empty? availables)
           nil
-          (take 9 (for [value availables result (sudoku (set-cell m cell value)) :while result] result)))))))
-
-
-;(defn sudoku [m]
-;  (pretty-matrix m)
-;  (println "\n----------------\n")
-;  (if (empty? (empty-cells m))
-;      m
-;      (let [cell (first (empty-cells-ordered-by-contraints m))]
-;        (let [availables (apply available-values-for-cell (cons m cell))]
-;          (if (empty? availables)
-;            nil
-;            (take 9 (for [value availables result (sudoku (set-cell m cell value)) :while result] result)))))))
+          (take GRID_WIDTH
+                (for [value availables result (sudoku (set-cell m cell value)) :while result]
+                     result)))))))
